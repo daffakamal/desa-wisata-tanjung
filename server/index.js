f9f939cd455558394ab3b17efa75dfb2b22b711f
shluf <luthfisalis09@gmail.com>
2024-09-14 16:50:09 +0700

    feat: update page

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const kesenianRoutes = require('./data/routes/kesenianRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
