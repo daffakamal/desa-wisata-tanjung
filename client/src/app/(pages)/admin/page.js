709320ad94518e37edd2746a1cdca743ebcf9a89
shluf <luthfisalis09@gmail.com>
2024-10-27 21:45:56 +0700

    add logout

--- client/src/app/(pages)/admin/page.js
+++ client/src/app/(pages)/admin/page.js
@@ -14,6 +14,7 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area"
 import AdminLayout from "./layout"
 import DialogUpload from "./upload"
+import { logout } from "@/app/auth/services/auth"
 
 
 const tabData = [
@@ -65,6 +66,10 @@ export default function DashboardDemo() {
       <header className="flex justify-between items-center mb-6 bg-gray-800 text-white p-4 rounded-lg">
         <h1 className="text-2xl font-bold">Dashboard Admin</h1>
         <nav>
+
+        <button className="flex items-center text-white" onClick={logout}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>
+        </button>
 
         </nav>
       </header>
